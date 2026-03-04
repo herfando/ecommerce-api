defe44fa62a4f2924374932bf05bf1b8b2412231
Herfando <herfando_91@yahoo.com>
2026-03-04 14:23:43 +0700

    new file:   src/controllers/authController.ts         new file:   src/middlewares/authMiddleware.ts         modified:   src/models/User.ts         new file:   src/routes/authRoutes.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,24 +1,14 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     role: {
         type: String,
-        enum: ["user", "admin"],
-        default: "user"
+        enum: ["buyer", "seller"],
+        default: "buyer"
     }
-}, { timestamps: true });
+}, { timestamps: true })
 
-export default mongoose.model("User", userSchema);
+export default mongoose.model("User", userSchema)
