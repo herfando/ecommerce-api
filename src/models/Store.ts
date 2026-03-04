281e2a3b990b0bd871c24a9872cab3e0e5f67188
Herfando <herfando_91@yahoo.com>
2026-03-04 14:33:25 +0700

    new file:   src/models/Store.ts         modified:   src/models/User.ts

---
+++ src/models/Store.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose"
+
+const storeSchema = new mongoose.Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    domain: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    address: {
+        city: { type: String, required: true },
+        postalCode: { type: String, required: true },
+        detail: { type: String, required: true }
+    }
+}, { timestamps: true })
+
+export default mongoose.model("Store", storeSchema)
