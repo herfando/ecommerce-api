143660ce697e74744118d56c2979dccf1d7cd7f2
Herfando <herfando_91@yahoo.com>
2026-03-05 15:11:43 +0700

    edit server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,15 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 mongoose.connect(process.env.MONGO_URI as string)
     .then(() => {
         console.log("MongoDB Connected");
-        app.listen(PORT, () => {
+
+        app.listen(PORT, "0.0.0.0", () => {
             console.log(`Server running on port ${PORT}`);
         });
+
     })
     .catch((err) => console.log(err));
