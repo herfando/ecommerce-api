48f0522a33d66adb18363399c90676426fda1b59
Herfando <herfando_91@yahoo.com>
2026-03-06 21:26:20 +0700

    deleted dist

--- dist/routes/productRoutes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { getProducts, createProduct } from "../controllers/productController.js";
-const router = Router();
-router.get("/", getProducts);
-router.post("/", createProduct);
-export default router;
